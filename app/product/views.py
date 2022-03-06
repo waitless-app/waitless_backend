f642ad869dee3db73a70e9b1bbf1c8739b23a79e
Sebastian Wrzalek <s.wrzalek@gmail.com>
2022-03-06 17:18:01 +0100

    fix(product) 'rest_framework.filters' imported but unused

--- app/product/views.py
+++ app/product/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status, filters
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
